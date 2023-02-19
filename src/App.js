0b0552d6d1d61c2976945d317d0ad848f829f7ba
Kaushik Mondal <kaushik@kaushiks-mbp.lan>
2023-02-19 16:55:37 -0500

    contact done

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ function App() {
 			<a href="/" class="text-white mr-6 font-bold">Kaushik Mondal</a>
 			<ul className="flex">
 				<li className="inline-block mr-4">
-					<a href="" className="text-white font-medium hover:text-yellow-200">About</a>
+					<a href="#about" className="text-white font-medium hover:text-yellow-200">About</a>
 				</li>
 				<li className="inline-block mr-4">
 					<a href="#skills" className="text-white font-medium hover:text-yellow-200">Skills</a>
 				</li>
 				<li class="inline-block mr-4">
-					<a href="" className="text-white font-medium hover:text-yellow-200">Projects</a>
+					<a href="#projects" className="text-white font-medium hover:text-yellow-200">Projects</a>
 				</li>
 				<li class="inline-block mr-4">
-					<a href="" className="text-white font-medium hover:text-yellow-200">Contact</a>
+					<a href="#contact" className="text-white font-medium hover:text-yellow-200">Contact</a>
 				</li>
 				
 			</ul>
@@ -154,7 +154,7 @@ function App() {
 				<h1 className='mt-20 text-4xl mr-12 text-right'>Projects</h1>
 				<div className="flex flex-wrap justify-center p-10">
 					<div className="flex flex-col w-1/3 m-8">
-						<img src="images/career_tracker.png" alt="project image" />
+						<img src="images/career_tracker.png" alt="project" />
 						<h1 className="text-4xl py-4 text-yellow-100">Career Tracker</h1>
 						<div className="flex">
 							<svg viewBox="0 0 128 128" width="40" height="40">
@@ -189,7 +189,7 @@ function App() {
 						</div>
 					</div>
 					<div className="flex flex-col w-1/3 m-8">
-						<img src="images/career_tracker.png" alt="project image" />
+						<img src="images/career_tracker.png" alt="project" />
 						<h1 className="text-4xl py-4 text-yellow-100">Career Tracker</h1>
 						<div className="flex">
 							<svg viewBox="0 0 128 128" width="40" height="40">
@@ -228,20 +228,25 @@ function App() {
 			<section id="contact">
 				<h1 className='mt-20 text-4xl ml-12'>Contact Me</h1>
 				<div>
-					<h3 className="ml-12 text-yellow-100 text-bold text-2xl my-8">Email me with a preformatted Gmail message:</h3>
-						<a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=example@gmail.com&su=job&body=hire" target='_blank' className="bg-red-400 py-3 px-6 ml-24 rounded text-bold">
-							<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" className="w-6 inline mr-4"/>
-							Gmail With Preformatted Message
-						</a>
-					<h3 className="ml-12 text-yellow-100 text-bold text-2xl my-8">Email without a template:</h3>
+					<h3 className="ml-12 text-yellow-100 text-bold text-2xl my-8">Email me with a preformatted message:</h3>
+					<a rel="noreferrer" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=kaushikmondal1717@gmail.com&su=Portfolio%20Connection&body=Dear%20Kaushik%2C%0A%0AHey%2C%20I%20saw%20your%20portfolio%20and%20would%20like%20to%20work%20together%20or%20have%20a%20job%20opportunity%20for%20you.%0A%0ABest%20Regards%2C%20%0A%0A%3CYour%20Name%20Here%3E" target='_blank' className="bg-red-400 py-3 px-6 ml-24 rounded text-bold">
+						<img alt="Google icon" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" className="w-6 inline mr-4"/>
+						Gmail With Preformatted Message
+					</a>
+					<a className="bg-red-400 py-3 px-6 ml-4 rounded text-bold" href="https://outlook.live.com/owa/?path=/mail/action/compose&to=kaushikmondal1717@gmail.com&subject=Portfolio%20Connection&body=Dear%20Kaushik%2C%0A%0AHey%2C%20I%20saw%20your%20portfolio%20and%20would%20like%20to%20work%20together%20or%20have%20a%20job%20opportunity%20for%20you.%0A%0ABest%20Regards%2C%20%3CYour%20Name%20Here%3E">Outlook</a>
+
+					<a rel="noreferrer" href="mailto:kaushikmondal1717@gmail.com" target='_blank' className="bg-red-400 py-3 px-6 ml-4 rounded text-bold">
+						Email without Gmail
+					</a>
+					<h3 className="ml-12 text-yellow-100 text-bold text-2xl my-8">Copy and paste:</h3>
 					<p className="ml-24 text-xl">kaushikmondal1717@gmail.com</p>
 					<h3 className="ml-12 text-yellow-100 text-bold text-2xl my-8">Socials:</h3>
-					<a href="https://www.linkedin.com/in/kaushikdev/" target="_blank">
+					<a rel="noreferrer" href="https://www.linkedin.com/in/kaushikdev/" target="_blank">
 						<svg viewBox="0 0 128 128" className="inline w-20 ml-24">
 							<path fill="#0076b2" d="M116 3H12a8.91 8.91 0 00-9 8.8v104.42a8.91 8.91 0 009 8.78h104a8.93 8.93 0 009-8.81V11.77A8.93 8.93 0 00116 3z"></path><path fill="#fff" d="M21.06 48.73h18.11V107H21.06zm9.06-29a10.5 10.5 0 11-10.5 10.49 10.5 10.5 0 0110.5-10.49M50.53 48.73h17.36v8h.24c2.42-4.58 8.32-9.41 17.13-9.41C103.6 47.28 107 59.35 107 75v32H88.89V78.65c0-6.75-.12-15.44-9.41-15.44s-10.87 7.36-10.87 15V107H50.53z"></path>
 						</svg>
 					</a>
-					<a href="https://github.com/kaushikkm17" target="_blank">
+					<a rel="noreferrer" href="https://github.com/kaushikkm17" target="_blank">
 						<svg viewBox="0 0 128 128" className="inline w-20 ml-12">
 							<g fill="#555555"><path fill-rule="evenodd" clip-rule="evenodd" d="M64 5.103c-33.347 0-60.388 27.035-60.388 60.388 0 26.682 17.303 49.317 41.297 57.303 3.017.56 4.125-1.31 4.125-2.905 0-1.44-.056-6.197-.082-11.243-16.8 3.653-20.345-7.125-20.345-7.125-2.747-6.98-6.705-8.836-6.705-8.836-5.48-3.748.413-3.67.413-3.67 6.063.425 9.257 6.223 9.257 6.223 5.386 9.23 14.127 6.562 17.573 5.02.542-3.903 2.107-6.568 3.834-8.076-13.413-1.525-27.514-6.704-27.514-29.843 0-6.593 2.36-11.98 6.223-16.21-.628-1.52-2.695-7.662.584-15.98 0 0 5.07-1.623 16.61 6.19C53.7 35 58.867 34.327 64 34.304c5.13.023 10.3.694 15.127 2.033 11.526-7.813 16.59-6.19 16.59-6.19 3.287 8.317 1.22 14.46.593 15.98 3.872 4.23 6.215 9.617 6.215 16.21 0 23.194-14.127 28.3-27.574 29.796 2.167 1.874 4.097 5.55 4.097 11.183 0 8.08-.07 14.583-.07 16.572 0 1.607 1.088 3.49 4.148 2.897 23.98-7.994 41.263-30.622 41.263-57.294C124.388 32.14 97.35 5.104 64 5.104z"></path><path d="M26.484 91.806c-.133.3-.605.39-1.035.185-.44-.196-.685-.605-.543-.906.13-.31.603-.395 1.04-.188.44.197.69.61.537.91zm2.446 2.729c-.287.267-.85.143-1.232-.28-.396-.42-.47-.983-.177-1.254.298-.266.844-.14 1.24.28.394.426.472.984.17 1.255zM31.312 98.012c-.37.258-.976.017-1.35-.52-.37-.538-.37-1.183.01-1.44.373-.258.97-.025 1.35.507.368.545.368 1.19-.01 1.452zm3.261 3.361c-.33.365-1.036.267-1.552-.23-.527-.487-.674-1.18-.343-1.544.336-.366 1.045-.264 1.564.23.527.486.686 1.18.333 1.543zm4.5 1.951c-.147.473-.825.688-1.51.486-.683-.207-1.13-.76-.99-1.238.14-.477.823-.7 1.512-.485.683.206 1.13.756.988 1.237zm4.943.361c.017.498-.563.91-1.28.92-.723.017-1.308-.387-1.315-.877 0-.503.568-.91 1.29-.924.717-.013 1.306.387 1.306.88zm4.598-.782c.086.485-.413.984-1.126 1.117-.7.13-1.35-.172-1.44-.653-.086-.498.422-.997 1.122-1.126.714-.123 1.354.17 1.444.663zm0 0"></path></g>
 						</svg>
